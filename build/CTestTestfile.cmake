76b7f8b14d83fe379229301ae9143b0b4244f12c
Weijie Wang <duangsir518@163.com>
2025-04-04 14:11:13 +0200

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/wang/catkin_apritag/src
+# Build directory: /home/wang/catkin_apritag/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("apriltag_ros")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/wang/catkin_apritag/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/wang/forest_ws/install;/opt/ros/noetic;/opt/xbot'.split(';') if '/home/wang/forest_ws/install;/opt/ros/noetic;/opt/xbot' != '' else []
